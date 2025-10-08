ecfd7cbefea6f505803e901dd913dcc69dcf9bf8
Rif'an <rifanhabibi212205@gmail.com>
2025-10-08 21:34:55 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,53 @@
-public class Main{
-    public static void main(String[] args){
-        int[] arr = {1, 2, 3};
+package main;
+public class Main {
+    public static void main(String[] args) {
+        //deklarasi class emplloyee
+        Employee employee1 = new Employee();
+        Employee employee2 = new Employee(1, "Rif'an Habibi", 15000);
 
-        for(int num: arr){
-            System.out.println(num);
-        }
+        //menampilkan data
+        employee1.displayData();
+        System.out.println("Yearly salary: $" + employee1.yearSalary());
+        System.out.println("====================================================");
+        employee2.displayData();
+        System.out.println("Yearly salary: $" + employee1.yearSalary(400));
+    }
+}
+
+class Employee{ //class karyawan
+    //deklarasi private variable
+    private int id;
+    private String name;
+    private double salary;
+
+    //non-argument constructor
+    public Employee(){
+        id = 0;
+        name = "New Employee";
+        salary = 0.0;
+    }
+
+    //parameterized constructor
+    public Employee(int id, String name, double salary){
+        this.id = id;
+        this. name = name;
+        this.salary = salary;
+    }
+
+    //overloading methods: gaji tahunan tanpa bonus
+    public double yearSalary(){
+        return this.salary * 12;
+    }
+
+    //gaji tahunan dengan bonus
+    public double yearSalary(double bonuses){
+        return (this.salary + bonuses) * 12;
+    }
+
+    //menampilkan data karyawan
+    public void displayData(){
+        System.out.println("Employee id: " + this.id);
+        System.out.println("Name: " + this.name);
+        System.out.println("Salary: $" + this.salary);
     }
 }
